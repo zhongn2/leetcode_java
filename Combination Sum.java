162ad6e151cf8dfee39de520df673a2e663b7386
zhongn2 <zhongn2@miamioh.edu>
2015-02-26 17:01:43 -0500

    combination sum

---
+++ Combination Sum.java
@@ -0,0 +1,29 @@
+//Combination Sum
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        if(candidates == null || candidates.length == 0){
+            return result;
+        }
+        comSumhelper(candidates, target, result, path, 0);
+        return result;
+    }
+    public void comSumhelper(int[] cans, int target, List<List<Integer>> result,  ArrayList<Integer> path, int pos){
+        if(target == 0){
+            result.add(new  ArrayList<Integer>(path));
+            return;
+        }
+        for(int i = pos; i < cans.length; i++){
+            if(target < cans[i]){
+                break;
+            }
+            path.add(cans[i]);
+            comSumhelper(cans, target - cans[i], result, path, i);
+            path.remove(path.size() - 1);
+            
+        }
+    }
+}
---
+++ Intersection of Two Linked Lists.java
@@ -0,0 +1,3 @@
+// the solution takes some time to figure out
+// we know that if there is a common part in the intersection
+// we should know the diff between two linked list
